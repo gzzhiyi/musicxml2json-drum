9b3465f00886b9d90703605d51b0b02293c5af6b
Hezhiyi <hezhiyi@rantion.com>
2025-03-05 12:53:19 +0800

    First commit.

---
+++ README.md
@@ -0,0 +1,15 @@
+# MusicXML2JSON Drum
+
+## Install
+
+### NPM install
+
+  ```bash
+    npm i musicxml2json-drum
+  ```
+
+### Yarn install
+
+  ```bash
+    yarn add musicxml2json-drum
+  ```
---
+++ build/rollup.dev.config.js
@@ -0,0 +1,56 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import eslint from '@rollup/plugin-eslint'
+import replace from '@rollup/plugin-replace'
+import json from '@rollup/plugin-json'
+import typescript from 'rollup-plugin-typescript2'
+import del from 'rollup-plugin-delete'
+import path from 'path'
+
+const getPath = _path => path.resolve(__dirname, _path)
+const extensions = [
+  '.js',
+  '.ts',
+  '.tsx'
+]
+
+export default {
+  input: getPath('../src/index.ts'),
+  external: [
+    'react',
+    'react-dom'
+  ],
+  output: [
+    {
+      name: 'musicxml2json-drum',
+      file: 'demos/dist/index.js', // 通用模块
+      format: 'umd',
+    },
+    {
+      name: 'musicxml2json-drum',
+      file: 'demos/dist/index.esm.js', // es6模块
+      format: 'esm',
+    }
+  ],
+  plugins: [
+    del({ targets: 'demos/dist/*' }),
+    replace({
+      preventAssignment: true,
+      babelHelpers: 'runtime',
+      values: {
+        'process.env.NODE_ENV': JSON.stringify('development')
+      }
+    }),
+    json(),
+    nodeResolve(extensions),
+    commonjs(),
+    eslint({
+      throwOnError: true,
+      include: ['src/**/*.ts'],
+      exclude: ['node_modules/**', 'dist/**', 'demos/**', 'build/**']
+    }),
+    typescript({
+      extensions
+    })
+  ]
+}
